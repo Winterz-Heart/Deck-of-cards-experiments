ec394639e6cdab31f45f0b57069b4dac59356eed
Winterz-Heart <duncan47685@gmail.com>
2025-05-19 12:00:17 +0000

    add initial deck of cards generation script

---
+++ deck of cards genertaion.py
@@ -0,0 +1,7 @@
+class.DECK:
+    def __init__(self, suits, ranks, card):
+        suits = ["C", "D", "H", "S"]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K," "A"]
+        self.card = [suit, rank for suit and suits for rank in rank]
+
+
