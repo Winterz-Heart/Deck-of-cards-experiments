e2567e836579d525e1e16115ab7ed29217409fa2
Winterz-Heart <duncan47685@gmail.com>
2025-05-19 12:19:04 +0000

    added random (1 random card) and hand (7 unique random cards) functions to the class deck

--- deck of cards genertaion.py
+++ deck of cards genertaion.py
@@ -1,7 +1,25 @@
-class.DECK:
-    def __init__(self, suits, ranks, card):
+import random
+
+class DECK:
+    # Generate the whole deck
+    def __init__(self):
         suits = ["C", "D", "H", "S"]
-        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K," "A"]
-        self.card = [suit, rank for suit and suits for rank in rank]
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        self.card = [(suit+rank) for suit in suits for rank in ranks]
+
+    # 1 Random card
+    def random(self):
+        return random.choice(self.card)
+    
+    # 1 seven card hand
+    def hand(self):
+        return random.sample(self.card, 7)
 
 
+
+
+
+
+# Print the whole deck
+deck = DECK()
+print(deck.hand())
